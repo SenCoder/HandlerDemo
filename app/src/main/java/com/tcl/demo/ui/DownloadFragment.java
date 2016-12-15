5421863282c2d629e823f63084ee7bfc5bd64f8e
Sheng Yuan <neo_yuan@outlook.com>
2016-12-15 10:34:58 +0800

    Add fragment

--- app/src/main/java/com/tcl/demo/ui/DownloadFragment.java
+++ app/src/main/java/com/tcl/demo/ui/DownloadFragment.java
@@ -1,6 +1,14 @@
 package com.tcl.demo.ui;
 
-import android.app.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tcl.handler.demo.R;
 
 /**
  * Created by shengyuan on 16-12-15.
@@ -8,6 +16,11 @@ import android.app.Fragment;
 
 public class DownloadFragment extends Fragment {
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_download, container, false);
 
-
+        return v;
+    }
 }
--- app/src/main/java/com/tcl/demo/ui/MainActivity.java
+++ app/src/main/java/com/tcl/demo/ui/MainActivity.java
@@ -1,55 +1,29 @@
 package com.tcl.demo.ui;
 
-import android.os.Handler;
-import android.os.Message;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.widget.Toast;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 
 import com.tcl.handler.demo.R;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private Handler mHandler = new Handler() {
-
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            Toast.makeText(MainActivity.this, "seconds " + msg.arg1, Toast.LENGTH_SHORT).show();
-
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startThread();
+
+        Fragment fragment = new DownloadFragment();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.add(R.id.fragment_container, fragment);
+        transaction.commit();
+
     }
 
-    private void startThread() {
-        new Thread(mRunnable).start();
-    }
-
-    Runnable mRunnable = new Runnable() {
-
-        private int seconds = 0;
-
-        @Override
-        public void run() {
-            try {
-                while (true) {
-                    Message msg = new Message();
-                    msg.arg1 = seconds ++;
-                    mHandler.sendMessage(msg);
-                    Thread.sleep(1000);
-                }
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    };
 
 }
